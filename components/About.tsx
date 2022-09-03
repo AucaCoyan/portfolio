17bdcfe983ec94a3edbff810749696d7876a2589
Auca Maillot <aucacoyan@gmail.com>
2022-09-03 09:45:37 -0300

    Next.JS-yfied a sample web. Separate some components

---
+++ components/About.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section id="about">
+      <div className="tile tile-1">
+        <div>
+          <h2>STACK</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus
+            tempore accusantium voluptates corrupti facere culpa necessitatibus,
+          </p>
+          <img src="img/react.png" alt="" width="200" />
+        </div>
+      </div>
+      <div className="tile tile-2">
+        <div>
+          <h2>ABOUT</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus
+            tempore accusantium voluptates corrupti facere culpa necessitatibus,
+          </p>
+          <img src="img/profile.png" alt="" width="100" height="100" />
+        </div>
+      </div>
+      <div className="tile tile-3">
+        <div>
+          <h2>REFERENCES</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus
+            tempore accusantium voluptates corrupti facere culpa necessitatibus,
+          </p>
+          <img src="img/microsoft.png" alt="" width="300" />
+        </div>
+      </div>
+      <div className="tile tile-4">
+        <div>
+          <h2>SERVICES</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus
+            tempore accusantium voluptates corrupti facere culpa necessitatibus,
+          </p>
+          <img src="img/cloud.png" alt="" width="300" />
+        </div>
+      </div>
+      <div className="tile tile-5">
+        <div>
+          <h2>AWARDS</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus
+            tempore accusantium voluptates corrupti facere culpa necessitatibus,
+            nihil rem sequi deleniti quod repudiandae ratione nemo veniam sint,
+            dignissimos a repellendus nisi.
+          </p>
+          <img src="img/trophy.png" alt="" />
+        </div>
+      </div>
+    </section>
+  );
+}
